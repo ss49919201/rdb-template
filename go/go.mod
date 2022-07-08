b58acda8c80a0a360256c798583d430f2fa414db
s-beats <sakashin10291029@gmail.com>
2022-07-08 23:26:03 +0900

    gorm追加

--- go/go.mod
+++ go/go.mod
@@ -4,16 +4,20 @@ go 1.18
 
 require (
 	github.com/go-sql-driver/mysql v1.6.0
+	github.com/samber/lo v1.25.0
+	gorm.io/driver/mysql v1.3.4
+	gorm.io/gorm v1.23.8
 	xorm.io/xorm v1.3.1
 )
 
 require (
 	github.com/goccy/go-json v0.8.1 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.4 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
-	github.com/samber/lo v1.25.0 // indirect
 	github.com/syndtr/goleveldb v1.0.0 // indirect
 	golang.org/x/exp v0.0.0-20220303212507-bbda1eaf7a17 // indirect
 	xorm.io/builder v0.3.11-0.20220531020008-1bd24a7dc978 // indirect
---
+++ go/gorm/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+func main() {
+	_, err := gorm.Open(mysql.Open("user:password@tcp(localhost:3306)/rdb"), &gorm.Config{
+		Logger: logger.New(
+			log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
+			logger.Config{
+				SlowThreshold:             time.Second, // Slow SQL threshold
+				LogLevel:                  logger.Info, // Log level
+				IgnoreRecordNotFoundError: true,        // Ignore ErrRecordNotFound error for logger
+				Colorful:                  true,        // Disable color
+			},
+		),
+	})
+	if err != nil {
+		panic("failed to connect database")
+	}
+}
+
+func newMySQLClient() (*gorm.DB, error) {
+	return gorm.Open(mysql.Open("user:password@tcp(localhost:3306)/rdb)"), &gorm.Config{
+		Logger: logger.New(
+			log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
+			logger.Config{
+				SlowThreshold:             time.Second, // Slow SQL threshold
+				LogLevel:                  logger.Info, // Log level
+				IgnoreRecordNotFoundError: true,        // Ignore ErrRecordNotFound error for logger
+				Colorful:                  true,        // Disable color
+			},
+		),
+	})
+}
