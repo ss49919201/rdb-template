6c9869955322a8da7c7a7563cfb0693038efe4c6
s-beats <sakashin10291029@gmail.com>
2022-07-18 10:55:32 +0900

    haskell で クエリ実行

--- haskell/app/Main.hs
+++ haskell/app/Main.hs
@@ -2,8 +2,15 @@
 
 import Database.MySQL.Simple
 
-main :: IO ()
 main = do
-  conn <- connect defaultConnectInfo {connectHost = "127.0.0.1", connectUser = "user", connectPassword = "password", connectDatabase = "rdb"}
-  [Only i] <- query_ conn "select * from rdb.users"
-  print (i :: Int)
+  conn <- connect defaultConnectInfo {connectHost = "127.0.0.1", connectPort = 3306, connectUser = "user", connectPassword = "password", connectDatabase = "rdb"}
+  [Only i] <- ping conn
+  [Only i2] <- insert conn
+  print i
+  print i2
+
+ping :: Connection -> IO [Only Int]
+ping conn = query_ conn "select 2 + 2"
+
+insert :: Connection -> IO [Only Int]
+insert conn = query_ conn "INSERT INTO users (id) VALUES (`1`), (`2`), (`3`)"
