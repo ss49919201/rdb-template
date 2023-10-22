0aea83c74d9a44a8f0f311944ff3bc63b1fad723
ss49919201 <sakashin10291029@gmail.com>
2023-10-22 15:53:10 +0900

    query test

--- go/sqlx/query_test.go
+++ go/sqlx/query_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"testing"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -22,13 +21,39 @@ func TestQueryWithPrepare(t *testing.T) {
 		assert.Fail(t, "failed to insert row")
 	}
 	defer func() {
-		db.Exec("DROP TABLE IF EXISTS users")
+		db.Exec("DROP TABLE IF EXISTS tbl")
 	}()
 
 	t.Run("Type is int64", func(t *testing.T) {
 		result := QueryWithPrepare(db)
 		if assert.Greater(t, len(result), 0) {
-			assert.Equal(t, "int64", fmt.Sprintf("%T", result[0]["id"]))
+			_, ok := result[0]["id"].(int64)
+			assert.True(t, ok)
+		}
+	})
+}
+
+func TestQuery(t *testing.T) {
+	db, err := sqlx.Open("mysql", testDSN)
+	if err != nil {
+		assert.Fail(t, "failed to open database handle")
+	}
+
+	if _, err := db.Exec("CREATE TABLE IF NOT EXISTS tbl (id INT NOT NULL, PRIMARY KEY (id))"); err != nil {
+		assert.Fail(t, "failed to create table")
+	}
+	if _, err := db.Exec("INSERT INTO tbl (id) VALUES(1)"); err != nil {
+		assert.Fail(t, "failed to insert row")
+	}
+	defer func() {
+		db.Exec("DROP TABLE IF EXISTS tbl")
+	}()
+
+	t.Run("Type is int64", func(t *testing.T) {
+		result := Query(db)
+		if assert.Greater(t, len(result), 0) {
+			_, ok := result[0]["id"].([]byte)
+			assert.True(t, ok)
 		}
 	})
 }
